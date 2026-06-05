c411f669b2ed3d634d27cbe7cd516ec8e2770def
Narayana <venkatanarayana.charani@gmail.com>
2026-06-05 17:10:49 +0530

    diagnose: add startup exception catch-and-print wrapper

--- resume-parser-backend/src/main/java/com/examportal/ResumeParserApplication.java
+++ resume-parser-backend/src/main/java/com/examportal/ResumeParserApplication.java
@@ -7,6 +7,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ResumeParserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ResumeParserApplication.class, args);
+		try {
+			SpringApplication.run(ResumeParserApplication.class, args);
+		} catch (Throwable t) {
+			System.err.println("=== STARTUP EXCEPTION DETECTED ===");
+			t.printStackTrace();
+			Throwable cause = t;
+			int depth = 0;
+			while (cause != null && depth < 20) {
+				System.err.println("--- Depth " + depth + " cause: " + cause.getClass().getName() + " ---");
+				System.err.println(cause.getMessage());
+				cause = cause.getCause();
+				depth++;
+			}
+			System.err.println("=== END OF STARTUP EXCEPTION DETAILS ===");
+			throw t;
+		}
 	}
 }
